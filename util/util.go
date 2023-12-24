3c5d2c8fa1a906ad907191a9ee1d2699f53a6583
aliusman3 <aliusman248s@gmail.com>
2023-12-24 18:54:58 +0500

    feat: create util module

---
+++ util/util.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func ReadInput(filePath string) []string {
+	input, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input.Close()
+
+	inputArray := []string{}
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		inputArray = append(inputArray, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return inputArray
+}
+
+func CreateOutput() *os.File {
+	output, err := os.Create("output.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return output
+}
+
+func GetLogger(w *os.File) func(s string, ags ...any) {
+	return func(s string, ags ...any) {
+		fmt.Fprintf(w, s, ags...)
+	}
+}
