e65d9110ce2702e2952d51d0623775aff6299766
aliusman3 <aliusman248s@gmail.com>
2023-12-24 16:42:32 +0500

    Initial commit

---
+++ 1-trebuchet.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+var digitStrings = map[string]int{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+func findStringDigit(line string, idx int, lineSize int, reverse bool, w io.Writer) (int, bool) {
+	sub := ""
+	if idx+5 <= lineSize {
+		sub = line[idx : idx+5]
+	} else {
+		sub = line[idx:]
+	}
+	for k, v := range digitStrings {
+		if strings.Contains(sub, k) && !(strings.HasPrefix(sub, k) || strings.HasSuffix(sub, k)) {
+			fmt.Fprintf(w, " substring match but no prefix or suffix %s ", sub)
+		}
+		if !reverse && strings.HasPrefix(sub, k) {
+			fmt.Fprintf(w, " found prefix for sub %s", sub)
+			return v, true
+		}
+
+		if reverse && strings.HasSuffix(sub, k) {
+			fmt.Fprintf(w, " found suffix for sub %s", sub)
+			return v, true
+		}
+	}
+	return -1, false
+}
+
+func main() {
+	sum := 0
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	result, err := os.Create("result.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer result.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			fmt.Fprintf(result, "Found empty string at index %d\n", lines)
+			continue
+		}
+		fmt.Fprintf(result, "lines %d", lines)
+		fmt.Fprintf(result, " line %s ", line)
+		firstDigit := -1
+		lastDigit := -1
+		lineSize := len(line)
+		for i, c := range line {
+			if unicode.IsDigit(c) {
+				firstDigit = int(c - '0')
+				break
+			} else if sd, found := findStringDigit(line, i, lineSize, false, result); found {
+				firstDigit = sd
+				break
+			}
+		}
+
+		for i := range line {
+			j := lineSize - 1 - i
+			c := rune(line[j])
+			if unicode.IsDigit(c) {
+				lastDigit = int(c - '0')
+				break
+			} else if sd, found := findStringDigit(line, j, lineSize, true, result); found {
+				lastDigit = sd
+				break
+			}
+		}
+
+		fmt.Fprintf(result, "\nfirstDigit %d lastDigit %d\n", firstDigit, lastDigit)
+		val := firstDigit*10 + lastDigit
+		fmt.Fprintf(result, "Val %d \n", val)
+		sum += val
+		lines++
+		fmt.Fprintln(result)
+	}
+
+	fmt.Println("sum", sum)
+}
